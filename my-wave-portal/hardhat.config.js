825113491c220328fa5f7065fd3788b5540fb451
LiCrazy (Fun <35451692+henry412@users.noreply.github.com>
2022-11-28 01:42:52 +0000

    builderspace

---
+++ my-wave-portal/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+// Import and configure dotenv
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
